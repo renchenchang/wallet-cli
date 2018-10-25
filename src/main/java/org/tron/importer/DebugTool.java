8bc676df46e900657068ab658e7d1b12a4060f11
renchenchang <rencc@sina.com>
2018-10-25 21:14:14 +0800

    load transacti info

---
+++ src/main/java/org/tron/importer/DebugTool.java
@@ -0,0 +1,41 @@
+package org.tron.importer;
+
+import com.alibaba.fastjson.JSONObject;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.common.xcontent.XContentType;
+
+public class DebugTool {
+  private ConnectionTool connectionTool = new ConnectionTool();
+
+  public void LoadData() {
+    try {
+      Statement statement = connectionTool.getConn().createStatement();
+      ResultSet results = statement
+          .executeQuery("select hash from smart_contract_triggers ");
+      while (results.next()) {
+        String txid = results.getString(1);
+        UpdateRequest updateRequest = new UpdateRequest("smart_contract_triggers",
+            "smart_contract_triggers", txid);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("need_result", 1);
+        updateRequest.doc(jsonObject.toJSONString(), XContentType.JSON);
+        connectionTool.blockBulk.add(updateRequest);
+        if (connectionTool.blockBulk.numberOfActions() >= 10000) {
+          connectionTool.bulkSave();
+        }
+      }
+      if (connectionTool.blockBulk.numberOfActions() > 0) {
+        connectionTool.bulkSave();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    DebugTool debugTool = new DebugTool();
+    debugTool.LoadData();
+  }
+}
